3cf1cb1d2c9a2b92a44e2fd9566f3e50d6aaadc2
VivekHande16 <Vivekhande16sep@gmail.com>
2022-08-13 20:18:16 +0530

    updated database file

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,11 +3,18 @@ const salons = require('../model/salonsModel');
 const users = require('../model/userModel');
 require('dotenv').config();
 
+// const db = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+// });
+
 const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: 'remotemysql.com',
+  user: 'u2OZWuFU4K',
+  password: 'aJedyc8vcY',
+  database: 'u2OZWuFU4K',
 });
 
 db.connect((err) => {
